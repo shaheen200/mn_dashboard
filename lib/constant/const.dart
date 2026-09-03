406670f4866cf0352726863a8b15f4984d1c712d
shaheen200 <mohamedbedosalah.2002@gmail.com>
2026-09-03 17:05:46 +0300

    edit_domain

--- lib/constant/const.dart
+++ lib/constant/const.dart
@@ -1,4 +1,4 @@
 // const domin = "http://localhost:8088";
-const domin = "https://dean-jpg-stretch-adaptor.trycloudflare.com";
+const domin = "https://absent-sarah-roulette-photographs.trycloudflare.com";
 const String vApp = '1';
 final headApi = {'Content-Type': 'application/json', 'v-app': vApp};
